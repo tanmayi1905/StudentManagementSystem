b6f6d810b854c7419743986edd7a29d9393f652c
tanmayi <tanmayinagare20@gmail.com>
2026-07-07 16:26:28 +0530

    Added to student service

---
+++ src/com/tanu/sms/StudentService.java
@@ -0,0 +1,11 @@
+package com.tanu.sms;
+
+import java.util.ArrayList;
+import com.tanu.sms.Student;
+
+public class StudentService {
+
+    // List to store Student objects
+    private ArrayList<Student> studentList = new ArrayList<>();
+
+}
